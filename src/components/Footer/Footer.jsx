e5036fa64493ab1041d9adee922f7740c47f0756
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-12-27 13:27:08 +0100

    Creato footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+      &copy; {new Date().getFullYear()} | made with ❤️ by
+      <a
+        href="https://github.com/EliaGuarnieri?tab=repositories"
+        rel="noreferrer"
+        target="_blank"
+      >Elia Guarnieri</a>
+    </p>
+    </footer>
+  )
+}
+
+export default Footer
