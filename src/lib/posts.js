aefa644339662d749e453d6071568d57ea5e04c8
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-12-27 13:32:17 +0100

    Rimossi campi non utilizzati

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -45,67 +45,62 @@ export async function getPostBySlug(slug) {
 
   const post = [postData?.data.post].map(mapPostData)[0];
 
-  // If the SEO plugin is enabled, look up the data
-  // and apply it to the default settings
-
-  if (process.env.WORDPRESS_PLUGIN_SEO === true) {
-    try {
-      seoData = await apolloClient.query({
-        query: QUERY_POST_SEO_BY_SLUG,
-        variables: {
-          slug,
-        },
-      });
-    } catch (e) {
-      console.log(`[posts][getPostBySlug] Failed to query SEO plugin: ${e.message}`);
-      console.log('Is the SEO Plugin installed? If not, disable WORDPRESS_PLUGIN_SEO in next.config.js.');
-      throw e;
-    }
-
-    const { seo = {} } = seoData?.data?.post;
-
-    post.metaTitle = seo.title;
-    post.metaDescription = seo.metaDesc;
-    post.readingTime = seo.readingTime;
-
-    // The SEO plugin by default includes a canonical link, but we don't want to use that
-    // because it includes the WordPress host, not the site host. We manage the canonical
-    // link along with the other metadata, but explicitly check if there's a custom one
-    // in here by looking for the API's host in the provided canonical link
-
-    if (seo.canonical && !seo.canonical.includes(apiHost)) {
-      post.canonical = seo.canonical;
-    }
-
-    post.og = {
-      author: seo.opengraphAuthor,
-      description: seo.opengraphDescription,
-      image: seo.opengraphImage,
-      modifiedTime: seo.opengraphModifiedTime,
-      publishedTime: seo.opengraphPublishedTime,
-      publisher: seo.opengraphPublisher,
-      title: seo.opengraphTitle,
-      type: seo.opengraphType,
-    };
-
-    post.article = {
-      author: post.og.author,
-      modifiedTime: post.og.modifiedTime,
-      publishedTime: post.og.publishedTime,
-      publisher: post.og.publisher,
-    };
-
-    post.robots = {
-      nofollow: seo.metaRobotsNofollow,
-      noindex: seo.metaRobotsNoindex,
-    };
-
-    post.twitter = {
-      description: seo.twitterDescription,
-      image: seo.twitterImage,
-      title: seo.twitterTitle,
-    };
+  try {
+    seoData = await apolloClient.query({
+      query: QUERY_POST_SEO_BY_SLUG,
+      variables: {
+        slug,
+      },
+    });
+  } catch (e) {
+    console.log(`[posts][getPostBySlug] Failed to query SEO plugin: ${e.message}`);
+    console.log('Is the SEO Plugin installed? If not, disable WORDPRESS_PLUGIN_SEO in next.config.js.');
+    throw e;
   }
+
+  const { seo = {} } = seoData?.data?.post;
+
+  post.metaTitle = seo.title;
+  post.metaDescription = seo.metaDesc;
+  post.readingTime = seo.readingTime;
+
+  // The SEO plugin by default includes a canonical link, but we don't want to use that
+  // because it includes the WordPress host, not the site host. We manage the canonical
+  // link along with the other metadata, but explicitly check if there's a custom one
+  // in here by looking for the API's host in the provided canonical link
+
+  if (seo.canonical && !seo.canonical.includes(apiHost)) {
+    post.canonical = seo.canonical;
+  }
+
+  post.og = {
+    author: seo.opengraphAuthor,
+    description: seo.opengraphDescription,
+    image: seo.opengraphImage,
+    modifiedTime: seo.opengraphModifiedTime,
+    publishedTime: seo.opengraphPublishedTime,
+    publisher: seo.opengraphPublisher,
+    title: seo.opengraphTitle,
+    type: seo.opengraphType,
+  };
+
+  post.article = {
+    author: post.og.author,
+    modifiedTime: post.og.modifiedTime,
+    publishedTime: post.og.publishedTime,
+    publisher: post.og.publisher,
+  };
+
+  post.robots = {
+    nofollow: seo.metaRobotsNofollow,
+    noindex: seo.metaRobotsNoindex,
+  };
+
+  post.twitter = {
+    description: seo.twitterDescription,
+    image: seo.twitterImage,
+    title: seo.twitterTitle,
+  };
 
   return {
     post,
--- src/lib/users.js
+++ src/lib/users.js
@@ -4,8 +4,6 @@ import parameterize from 'parameterize';
 
 import { QUERY_ALL_USERS, QUERY_ALL_USERS_SEO } from 'data/users';
 
-// const ROLES_AUTHOR = ['author', 'administrator'];
-
 /**
  * postPathBySlug
  */
@@ -82,35 +80,32 @@ export async function getAllUsers() {
   // If the SEO plugin is enabled, look up the data
   // and apply it to the default settings
 
-  if (process.env.WORDPRESS_PLUGIN_SEO === true) {
-    try {
-      seoData = await apolloClient.query({
-        query: QUERY_ALL_USERS_SEO,
-      });
-    } catch (e) {
-      console.log(`[users][getAllUsers] Failed to query SEO plugin: ${e.message}`);
-      console.log('Is the SEO Plugin installed? If not, disable WORDPRESS_PLUGIN_SEO in next.config.js.');
-      throw e;
-    }
 
-    users = users.map((user) => {
-      const data = { ...user };
-      const { id } = data;
-
-      const seo = seoData?.data?.users.edges.map(({ node = {} }) => node).find((node) => node.id === id)?.seo;
-
-      return {
-        ...data,
-        title: seo.title,
-        description: seo.metaDesc,
-        robots: {
-          nofollow: seo.metaRobotsNofollow,
-          noindex: seo.metaRobotsNoindex,
-        },
-        social: seo.social,
-      };
+  try {
+    seoData = await apolloClient.query({
+      query: QUERY_ALL_USERS_SEO,
     });
+  } catch (e) {
+    console.log(`[users][getAllUsers] Failed to query SEO plugin: ${e.message}`);
+    console.log('Is the SEO Plugin installed? If not, disable WORDPRESS_PLUGIN_SEO in next.config.js.');
+    throw e;
   }
+
+  users = users.map((user) => {
+    const data = { ...user };
+    const { id } = data;
+
+    const seo = seoData?.data?.users.edges.map(({ node = {} }) => node).find((node) => node.id === id)?.seo;
+
+    return {
+      ...data,
+      title: seo.title,
+      robots: {
+        nofollow: seo.metaRobotsNofollow,
+        noindex: seo.metaRobotsNoindex,
+      }
+    };
+  });
 
   return {
     users,
