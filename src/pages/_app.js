67c76c508de89f8f128762eb5f016aa38da07ed5
Elia Guarnieri <elia.guarnieri@gmail.com>
2021-12-27 13:32:33 +0100

    Aggiunto layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import 'normalize.css'
+import '../styles/globals.scss'
+
+import Layout from 'Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
