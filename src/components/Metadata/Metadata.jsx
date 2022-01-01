c9a6be5e70d7107538d5789146c7b16d3f2307fb
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-01 17:43:14 +0100

    Created Metadata

---
+++ src/components/Metadata/Metadata.jsx
@@ -0,0 +1,69 @@
+import Link from 'next/link';
+import Image from 'next/image'
+
+import { categoryPathBySlug } from 'lib/categories';
+import { authorPathByName } from 'lib/users';
+import { formatDate } from 'lib/datetime';
+
+import { FaMapPin } from 'react-icons/fa';
+import styles from './Metadata.module.scss';
+
+
+const Metadata = ({ author, date, categories, isSticky = false }) => {
+
+  return (
+    <ul className={styles.metadata}>
+      {author && (
+        <li className={styles.metadataAuthor}>
+          <address>
+            {author.avatar && (
+              <span className={styles.avatar}>
+                <Image
+                  width={author.avatar.width}
+                  height={author.avatar.height}
+                  src={author.avatar.url}
+                  alt="Author Avatar"
+                />
+              </span>
+            )}
+            By{' '}
+            <Link href={authorPathByName(author.name)}>
+              <a rel="author">{author.name}</a>
+            </Link>
+          </address>
+        </li>
+      )}
+      {date && (
+        <li>
+          <time pubdate="pubdate" dateTime={date}>
+            <small>{formatDate(date)}</small>
+          </time>
+        </li>
+      )}
+      {Array.isArray(categories) && categories[0] && (
+        <li className={styles.metadataCategories}>
+          {categories && (
+            <ul>
+              {categories.map((category) => {
+                return (
+                  <li key={category.slug}>
+                    <Link href={categoryPathBySlug(category.slug)}>
+                      <a>{category.name}</a>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </li>
+      )}
+      {isSticky && (
+        <li className={styles.metadataSticky}>
+          <FaMapPin aria-label="Sticky Post" />
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Metadata;
