c4052a127fd93bf014f34ea388ce788c41dab4ff
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-01 17:44:44 +0100

    Populated single page

--- src/pages/posts/[slug].jsx
+++ src/pages/posts/[slug].jsx
@@ -1,33 +1,55 @@
-import Image from 'next/image';
-
 import { getPostBySlug, getAllPosts } from 'lib/posts';
 import { formatDate } from 'lib/datetime';
 
-export default function Post({ post }) {
-  const {
-    title,
-    content,
-    modified,
-    featuredImage,
-  } = post;
+import Image from 'next/image';
+import Container from 'components/Container';
+import Metadata from 'components/Metadata';
+
+import styles from 'styles/Pages/Single.module.scss'
+
+export default function Post({ post, options = {} }) {
+  const { featuredImage, title, date, author, content, modified, categories } = post;
+  const { excludeMetadata = [] } = options;
+
+  const metadata = {};
+
+  if (!excludeMetadata.includes('author')) {
+    metadata.author = author;
+  }
+
+  if (!excludeMetadata.includes('date')) {
+    metadata.date = date;
+  }
+
+  if (!excludeMetadata.includes('categories')) {
+    metadata.categories = categories;
+  }
 
 
   return (
     <>
-      <header>
+      <header className={styles.header}>
         {featuredImage && (
           <Image
-            {...featuredImage}
+            className={styles.featuredImage}
             src={featuredImage.sourceUrl}
             alt={featuredImage.caption}
+            layout='responsive'
+            width={976}
+            height={549}
+            priority
           />
         )}
-        <h1>${title}</h1>
+        <Container className={styles.header__text}>
+          <h1>{title}</h1>
+          <Metadata {...metadata} />
+        </Container>
       </header>
+      <Container>
+        <section className={styles.content} dangerouslySetInnerHTML={{__html: content}} />
 
-      ${content}
-
-      <p>Last updated on {formatDate(modified)}.</p>
+        <p>Last updated on {formatDate(modified)}.</p>
+      </Container>
     </>
   );
 }
