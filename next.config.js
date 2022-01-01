0c3584ea122cda8f558b4ce40c0be5b123851124
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-01 17:42:51 +0100

    Added source for image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   env: {
     WORDPRESS_GRAPHQL_ENDPOINT: process.env.WORDPRESS_GRAPHQL_ENDPOINT
+  },
+
+  images: {
+    domains: ['headlesswp.altervista.org', 'secure.gravatar.com']
   }
 }
