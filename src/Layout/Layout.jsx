5e1e95f244bf58b58c02b66dabe597fdb8ba30a0
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-02 14:45:23 +0100

    Used Transition

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,14 +1,21 @@
+import { useRouter } from 'next/router'
+
 import Navbar from 'components/Navbar'
+import Transition from 'components/Transition'
 import Main from 'components/Main'
 import Footer from 'components/Footer'
 
 import styles from './Layout.module.scss'
 
 const Layout = ({children}) => {
+  const router = useRouter()
+
   return (
     <div className={styles.layoutContainer}>
       <Navbar />
-      <Main>{children}</Main>
+      <Transition location={router.asPath}>
+        <Main>{children}</Main>
+      </Transition>
       <Footer />
     </div>
   )
