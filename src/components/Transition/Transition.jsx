a6c3f3aa772a8f391dd07503ec1f7975ca329e06
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-02 19:17:26 +0100

    Better implementation

--- src/components/Transition/Transition.jsx
+++ src/components/Transition/Transition.jsx
@@ -1,24 +1,28 @@
+import { AnimatePresence } from 'framer-motion';
 import { motion } from "framer-motion";
 
 //import styles from './Transition.module.scss'
 
 const Transition = ({children, location}) => {
   return (
-    <motion.div
-      key={location}
-      initial="initial"
-      animate="animate"
-      variants={{
-        initial: {
-          opacity: 0,
-        },
-        animate: {
-          opacity: 1,
-        },
-      }}
+    <AnimatePresence
+      exitBeforeEnter
+      initial={false}
     >
-      {children}
-    </motion.div>
+      <motion.div
+        key={location}
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={{
+          hidden: { opacity: 0 },
+          enter: { opacity: 1 },
+          exit: { opacity: 0}
+        }}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
