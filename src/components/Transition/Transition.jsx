19dc1fe902cb6bd23bd8ac1da6e25b441a4182c7
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-02 14:44:56 +0100

    Created Transition

---
+++ src/components/Transition/Transition.jsx
@@ -0,0 +1,27 @@
+import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group'
+
+import styles from './Transition.module.scss'
+
+const TIMOUT = 200
+
+const Transition = ({children, location}) => {
+  return (
+    <TransitionGroup className={styles.transitionGroup}>
+      <ReactTransition
+        key={location}
+        timeout={{
+          enter: TIMOUT,
+          exit: TIMOUT
+        }}
+      >
+        {status => (
+          <div className={styles[status]}>
+            {children}
+          </div>
+        )}
+      </ReactTransition>
+    </TransitionGroup>
+  )
+}
+
+export default Transition
