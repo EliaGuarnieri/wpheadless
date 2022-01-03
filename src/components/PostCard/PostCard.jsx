84cda5ee6d201e4fb047516474cca9fcdddb3499
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-03 21:39:54 +0100

    Styled for mobile and desktop

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,58 +1,73 @@
-import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { useWindowSize, useBreakpoint } from 'hooks'
 
 import { postPathBySlug, sanitizeExcerpt } from 'lib/posts';
 
 import Metadata from 'components/Metadata';
+import FeaturedImage from 'components/FeaturedImage';
+import Button from 'components/Button'
 
-import { FaMapPin } from 'react-icons/fa';
 import styles from './PostCard.module.scss';
 
-const PostCard = ({ post, options = {} }) => {
-  const { title, excerpt, slug, date, author, categories, isSticky = false } = post;
-  const { excludeMetadata = [] } = options;
+const PostCard = ({ post }) => {
+  const { title, featuredImage, excerpt, slug, author, categories } = post;
 
-  const metadata = {};
+  const { windowWidth } = useWindowSize()
+  const { lg } = useBreakpoint()
 
-  if (!excludeMetadata.includes('author')) {
-    metadata.author = author;
-  }
+  const isDesktop = windowWidth ? windowWidth >= lg : false
 
-  if (!excludeMetadata.includes('date')) {
-    metadata.date = date;
-  }
+  const [imgSize, setImgSize] = useState({width: 400, height: 400})
 
-  if (!excludeMetadata.includes('categories')) {
-    metadata.categories = categories;
-  }
+  useEffect(() => {
+    if(isDesktop) return setImgSize({width: 400, height: 400})
 
-  let postCardStyle = styles.postCard;
+    setImgSize({width: lg, height: lg * 2 / 3})
+  }, [isDesktop, lg])
 
-  if (isSticky) {
-    postCardStyle = `${postCardStyle} ${styles.postCardSticky}`;
-  }
+  const { width, height } = imgSize
 
   return (
-    <div className={postCardStyle}>
-      {isSticky && <FaMapPin aria-label="Sticky Post" />}
-      <Link href={postPathBySlug(slug)}>
-        <a>
-          <h3
-            className={styles.postCardTitle}
+    <div className={styles.postCard}>
+      <FeaturedImage
+        className={styles.postCard__media}
+        src={featuredImage?.sourceUrl}
+        alt={featuredImage?.altText}
+        layout='responsive'
+        objectFit="cover"
+        width={width}
+        height={height}
+      />
+
+      <div className={styles.postCard__content}>
+        <div className={styles.postCard__body}>
+          <div className={styles.postCard__categories}>
+            <Metadata categories={categories} />
+          </div>
+          <h2
+            className={styles.postCard__title}
             dangerouslySetInnerHTML={{
               __html: title,
             }}
           />
-        </a>
-      </Link>
-      <Metadata {...metadata} />
-      {excerpt && (
-        <div
-          className={styles.postCardContent}
-          dangerouslySetInnerHTML={{
-            __html: sanitizeExcerpt(excerpt),
-          }}
-        />
-      )}
+          {excerpt && (
+            <div
+              className={styles.postCard__exerpt}
+              dangerouslySetInnerHTML={{
+                __html: sanitizeExcerpt(excerpt),
+              }}
+            />
+          )}
+          <div className={styles.postCard__author}>
+            <Metadata author={author} />
+          </div>
+        </div>
+        <div className={styles.postCard__footer}>
+          <Button href={postPathBySlug(slug)}>
+            Read article
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
