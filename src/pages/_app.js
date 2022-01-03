3bd5e147aeaeec8da88154dbc288d517b536240f
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-03 16:03:50 +0100

    Transition temp fix try

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,38 @@ import 'normalize.css'
 import '../styles/globals.scss'
 
 import Layout from 'Layout';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const routeChange = () => {
+      // Temporary fix to avoid flash of unstyled content
+      // during route transitions. Keep an eye on this
+      // issue and remove this code when resolved:
+      // https://github.com/vercel/next.js/issues/17464
+
+      const tempFix = () => {
+        console.log('tempFix')
+        const allStyleElems = document.querySelectorAll('style[media="x"]');
+        allStyleElems.forEach((elem) => {
+          elem.removeAttribute("media");
+        });
+      };
+      tempFix();
+    };
+
+    router.events.on("routeChangeComplete", routeChange );
+    router.events.on("routeChangeStart", routeChange );
+
+    return () => {
+      router.events.off('routeChangeComplete', routeChange)
+      router.events.off('routeChangeStart', routeChange)
+    }
+  })
+
   return (
     <Layout>
       <Component {...pageProps} />
