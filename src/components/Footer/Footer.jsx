a5e902f72d55cbb34218be3bec900ae0495871c9
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-05 19:11:20 +0100

    Inserita animazione cuore

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,13 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>
-      &copy; {new Date().getFullYear()} | made with ❤️ by
+      &copy; {new Date().getFullYear()} | made with <span className={styles.heart}></span> by
+      {' '}
       <a
         href="https://github.com/EliaGuarnieri?tab=repositories"
         rel="noreferrer"
         target="_blank"
-      > Elia Guarnieri</a>
+      >Elia Guarnieri</a>
     </p>
     </footer>
   )
