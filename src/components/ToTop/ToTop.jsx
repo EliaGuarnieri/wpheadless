b0377c542287263c5b2ac7526396a0d4c26c1815
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-29 18:04:51 +0100

    ToTop using Context

--- src/components/ToTop/ToTop.jsx
+++ src/components/ToTop/ToTop.jsx
@@ -1,11 +1,18 @@
-import styles from './ToTop.module.scss'
+import { useContext } from 'react'
+import { ScrollContext } from 'context'
 
 import { HiChevronUp as ToTopIcon } from 'react-icons/hi';
 
+import styles from './ToTop.module.scss'
 
-const ToTop = ({handleScroll}) => {
+const ToTop = () => {
+  const scrollbars = useContext(ScrollContext)
+
+  const scrollToTop = () => {
+    scrollbars.current.view.scroll({top: 0, behavior: 'smooth'})
+  }
   return (
-    <button className={styles.button} onClick={handleScroll}>
+    <button className={styles.button} onClick={scrollToTop}>
       <ToTopIcon className={styles.icon}/>
     </button>
   )
