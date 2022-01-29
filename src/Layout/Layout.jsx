ba83de53c84f3ae8dea47c09cad971ff41855e49
Elia Guarnieri <elia.guarnieri@gmail.com>
2022-01-29 14:46:49 +0100

    Aggiunto scroll to top

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,23 +1,31 @@
 import { useRouter } from 'next/router'
+import { useRef } from 'react'
 
 import Navbar from 'components/Navbar'
 import Transition from 'components/Transition'
 import Footer from 'components/Footer'
 import Scrollbars from 'components/Scrollbars'
+import ToTop from 'components/ToTop'
 
 import styles from './Layout.module.scss'
 
 const Layout = ({children}) => {
   const router = useRouter()
+  const scrollbars = useRef()
+
+  const handleScroll = () => {
+    scrollbars.current.view.scroll({top: 0, behavior: 'smooth'})
+  }
 
   return (
     <div className={styles.layoutContainer}>
       <Navbar />
       <Transition location={router.asPath}>
-        <Scrollbars>
+        <Scrollbars ref={scrollbars}>
           {children}
           <Footer />
         </Scrollbars>
+        <ToTop handleScroll={handleScroll}/>
       </Transition>
     </div>
   )
--- src/components/Scrollbars/Scrollbars.jsx
+++ src/components/Scrollbars/Scrollbars.jsx
@@ -1,10 +1,11 @@
 import { Scrollbars as ReactScrollbars } from 'react-custom-scrollbars-2'
+import { forwardRef } from 'react'
 
 import styles from './Scrollbars.module.scss'
 
-const Scrollbars = (props) => {
-  return (
+const Scrollbars = forwardRef((props, ref) => (
     <ReactScrollbars
+      ref={ref}
       renderThumbHorizontal={(props) => <div {...props} className={styles.thumb} />}
       renderThumbVertical={(props) => <div {...props} className={styles.thumb} />}
       hideTracksWhenNotNeeded={true}
@@ -12,7 +13,8 @@ const Scrollbars = (props) => {
     >
       { props.children }
     </ReactScrollbars>
-  )
-}
+))
+
+Scrollbars.displayName = 'Scrollbars'
 
 export default Scrollbars
